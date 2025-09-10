bef0a2d9451a15e6210e57afa488d4c2f522109f
kimuraasashin <24051902@d.anabuki-college.net>
2025-09-10 16:37:53 +0900

    木村慎
    Treasureスクリプトを変更

--- TeamB2025GameJam/Assets/kimura/Script/Treasure.cs
+++ TeamB2025GameJam/Assets/kimura/Script/Treasure.cs
@@ -3,6 +3,17 @@ using UnityEngine;
 public class Treasure : MonoBehaviour
 {
     public float GetTime = 2.0f;
+    private GameManager gameManager;
+
+    private void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    private void OnDestroy()
+    {
+        gameManager.Stolen();
+    }
     /*
     private void OnTriggerEnter(Collider other)
     {
